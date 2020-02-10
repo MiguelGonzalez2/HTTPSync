2a675a22606c3ef7d554f56fcc383fbb86b4050c
Zombor00 <thezombor@gmail.com>
2020-02-10 22:37:59 +0100

    Libreria con la pool de threads funcional

--- include/pool_threads.h
+++ include/pool_threads.h
@@ -1,13 +1,37 @@
 /**
 *Fuente de la libreria de getion de la pool de hilos. Proporciona una interfaz
-*para crear la pool, destruir la pool.
+*para crear la pool, destruir la pool y una función de prueba para los hilos.
 *
 *@author Alejandro Bravo, Miguel Gonzalez
 *@version 1.0
 *@date 08-02-2020
 **/
 
+#ifndef POOL_THREADS
+#define POOL_THREADS
+
 #include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 10 /*Cantidad de threads a crear en la pool*/
+
+typedef struct{
+    pthread_t tid[NUM_THREADS];
+    int num_threads;
+    int *num_working;
+    bool *stop;
+    pthread_mutex_t shared_mutex;
+    void *function;
+}pool_thread;
+
+typedef struct{
+    int *num_working;
+    bool *stop;
+    pthread_mutex_t shared_mutex;
+    void *function;
+}thread_args;
  
 /****
 *FUNCIÓN: pool_thread  *pool_th_ini()
@@ -25,4 +49,16 @@ pool_thread *pool_th_ini();
 *DESCRIPCION: Encargada de liberar los datos reservados para la pool de threads.
 *ARGS_OUT: none
 ****/
-void pool_th_destroy(pool_thread p_threads, int numThreads){
+void pool_th_destroy(pool_thread *p_threads, int numThreads);
+
+/****
+*FUNCIÓN: Funcion de prueba para los hilos.
+*ARGS_IN: void* args: Se le pasa un puntero a una estructura thread_args
+*que contiene argumentos de control del hiloargumentos de control del hilo.
+*DESCRIPCION: Funcion que prueba que los hilos y los argumentos
+*pasados a ellos funcionan correctamente.
+*ARGS_OUT: none
+****/
+void th_main(void *args);
+
+#endif 
